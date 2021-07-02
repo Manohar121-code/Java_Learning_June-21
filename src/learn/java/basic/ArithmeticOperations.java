e550bb107c82c91a7822370096a9fe892b8f29be
Manohar Sambali <42957019+Manohar51@users.noreply.github.com>
2021-07-02 09:33:54 +0530

    Initial commit

---
+++ src/learn/java/basic/ArithmeticOperations.java
@@ -0,0 +1,23 @@
+package learn.java.basic;
+
+public class ArithmeticOperations {
+	public static void main(String[] args) {
+		int i = 51;
+		int j = 10;
+		
+		int sum = i + j;
+		System.out.println("Sum - "+ sum);
+		
+		int sub = i - j;
+		System.out.println("Sub - "+ sub);
+		
+		int mul = i * j;
+		System.out.println("Mul - "+ mul);
+		
+		int div = i / j;
+		System.out.println("Div - "+ div);
+		
+		int rem = i % j;
+		System.out.println("Rem - "+ rem);
+	}
+}
---
+++ src/learn/java/basic/DataTypes.java
@@ -0,0 +1,43 @@
+package learn.java.basic;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+		//datatype variable = value;
+		int i = 20;
+		System.out.println(i);
+
+		System.out.println("int value - " + i);
+
+		byte b = 15;
+		System.out.println("byte value - " + b);
+
+		short s = 32767;
+		System.out.println("short value - " + s);
+
+		long l = 78687686;
+		System.out.println("long value - " + l);
+
+		char ch = '@';
+		System.out.println("char value - " + ch);
+
+		float f = 12.56f;
+		System.out.println("float value - " + f);
+
+		double d = 1636267.654655;
+		System.out.println("double value - " + d);
+
+		boolean bool = false;
+		System.out.println("boolean value - " + bool);
+
+		String str = "java";
+		System.out.println("string value - " + str);
+		
+		String firstName = "Mahesh";
+		
+		String onceUponATimeAKingWasThere = "something";
+		
+	}
+	
+}
